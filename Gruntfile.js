8d946caf8a2beb8457ffae29ee731524580115a7
Joel Alenchery <joel@alenchery.org>
2014-08-11 14:15:14 +0530

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-bump');
+
+  grunt.initConfig({
+    /**
+     * Increments the version number, etc.
+     */
+    bump: {
+      options: {
+        files: [
+          "package.json"
+        ],
+        commit: true,
+        commitMessage: 'chore(release): v%VERSION%',
+        commitFiles: [
+          "package.json"
+        ],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true
+      }
+    },    
+    
+  });
+
+  
+
+};
